f4e7bd97e1e79f8e662027e78f2fcfefb21bfb6c
DonyoBen <saquinbenjie7@gmail.com>
2025-09-16 01:28:57 +0800

    WIP on feature/game-board

---
+++ src/components/Board.tsx
@@ -0,0 +1,19 @@
+export default function GameBoard()
+{
+
+    return(
+        <>
+            <div className="grid grid-cols-3 gap-0.5">
+                <div className="bg-white p-10"> </div>
+                <div className="bg-white p-10"> </div>
+                <div className="bg-white p-10"> </div>
+                <div className="bg-white p-10"> </div>
+                <div className="bg-white p-10"> </div>
+                <div className="bg-white p-10"> </div>
+                <div className="bg-white p-10"> </div>
+                <div className="bg-white p-10"> </div>
+                <div className="bg-white p-10"> </div>
+            </div>
+        </>
+    )
+}
