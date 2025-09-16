c6a089fe4cd937ae53d5095ab8dc48cdc1d40601
DonyoBen <saquinbenjie7@gmail.com>
2025-09-16 11:56:27 +0800

    added button component

---
+++ src/components/button.tsx
@@ -0,0 +1,13 @@
+export default function Button() {
+
+    return (
+        <div className="flex space-x-6">
+            <button className="text-black rounded-xl shadow-lg h-25 w-25 p-5">
+                X
+            </button>
+            <button className="text-black rounded-xl shadow-lg h-25 w-25 p-5">
+                O
+            </button>
+        </div>
+    )
+}
