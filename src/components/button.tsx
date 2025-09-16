cb9e2d8b6aa535acc41b435f3199e162ea8bef92
DonyoBen <saquinbenjie7@gmail.com>
2025-09-16 13:06:25 +0800

    add route to the game

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,13 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function Button() {
+    const navigate = useNavigate();
+
+    function handlePlayerSelect(player: 'X' | 'O') {
+        // Redirect to /game and pass the selected player as state
+        navigate('/game', { state: { firstPlayer: player } });
+    }
 
     return (
         <div className="flex space-x-6">
-            <button className="text-black rounded-xl shadow-lg h-25 w-25 p-5">
+            <button
+                className="text-black rounded-xl shadow-lg h-25 w-25 p-5"
+                onClick={() => handlePlayerSelect('X')}
+            >
                 X
             </button>
-            <button className="text-black rounded-xl shadow-lg h-25 w-25 p-5">
+            <button
+                className="text-black rounded-xl shadow-lg h-25 w-25 p-5"
+                onClick={() => handlePlayerSelect('O')}
+            >
                 O
             </button>
         </div>
-    )
+    );
 }
