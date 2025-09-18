14de55d79c07295a53f031225860fd6ec80f4377
DonyoBen <saquinbenjie7@gmail.com>
2025-09-18 20:04:51 +0800

    refactor button code

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Button() {
     const navigate = useNavigate();
-
+    const playerSelection: string [] = ['X', 'O']
     function handlePlayerSelect(player: 'X' | 'O') {
         // Redirect to /game and pass the selected player as state
         navigate('/game', { state: { firstPlayer: player } });
@@ -10,18 +10,14 @@ export default function Button() {
 
     return (
         <div className="flex space-x-6">
-            <button
-                className="text-black rounded-xl shadow-lg h-25 w-25 p-5"
-                onClick={() => handlePlayerSelect('X')}
-            >
-                X
-            </button>
-            <button
-                className="text-black rounded-xl shadow-lg h-25 w-25 p-5"
-                onClick={() => handlePlayerSelect('O')}
-            >
-                O
-            </button>
+            {playerSelection.map((value) => (
+                <button
+                    className="text-black rounded-xl shadow-lg h-25 w-25 p-5"
+                    onClick={() => handlePlayerSelect('X')}
+                >
+                    {value}
+                </button>
+            ))}
         </div>
     );
 }
