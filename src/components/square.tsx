cd4eb52be9aac9af5c4bb7b69b94d10cdbb4bbae
DonyoBen <saquinbenjie7@gmail.com>
2025-09-18 13:03:34 +0800

    refactor UI code

--- src/components/square.tsx
+++ src/components/square.tsx
@@ -1,6 +1,14 @@
-export default function Square({ value, onSquareClick }) {
+type SquareProps = {
+    value: string | null;
+    onSquareClick: () => void; 
+};
+
+export default function Square({ value, onSquareClick } : SquareProps) {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button 
+      className="flex items-center justify-center text-4xl font-bold text-gray-700 border w-25 h-25 border-gray-300" 
+      onClick={onSquareClick}
+    >
       {value}
     </button>
   );
